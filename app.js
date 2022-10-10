d0480903177a1735c0710aaf1360e3c43afde289
HugoSanche <hbmclaren@gmail.com>
2022-10-09 21:03:11 -0500

    End install and initialized passport & session

--- app.js
+++ app.js
@@ -8,8 +8,13 @@ const mongoose = require("mongoose");
 //const encrypt =require("mongoose-encryption")
 
 //const sha =require("js-sha512");
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+//const bcrypt = require('bcrypt');
+//const saltRounds = 10;
+
+
+var session=require("express-session");
+var passport=require("passport");
+var passportLocalMongoose=require("passport-local-mongoose")
 
 const app=express();
 
@@ -21,6 +26,16 @@ app.use(bodyParser.urlencoded({
   extended:true
 }));
 
+app.use(session({
+  secret: "Un secreto",
+  resave: false,
+  saveUninitialized:false
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser: true});
 
 //for encryp i need to create the schema in this form
@@ -31,10 +46,18 @@ const userSchema= new mongoose.Schema({
 
 //const secret="esteesnuetrosecretoid."; // like a key to encript
 //userSchema.plugin(encrypt,{secret: process.env.SECRET_KEY, encryptedFields: ["password"]}); //use key and only encryp fields "password"
-
-
+userSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
+
 
 app.get("/",function(req,res){
   res.render("home");
@@ -47,50 +70,48 @@ app.get("/register",function(req,res){
 })
 
 app.post("/register",function(req, res){
-  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-    newUser= new User({
-      email: req.body.username,
-      password: hash
-    })
-
-    //when save field automatically the field password was encryted
-    newUser.save(function(err){ //saving new register
-      if(!err){
-        res.render("secrets");
-      }
-        else{
-          res.render("Something fail "+err);
-        }
-    });
-  });
-
-
+  // bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+  //   newUser= new User({
+  //     email: req.body.username,
+  //     password: hash
+  //   })
+  //
+  //   //when save field automatically the field password was encryted
+  //   newUser.save(function(err){ //saving new register
+  //     if(!err){
+  //       res.render("secrets");
+  //     }
+  //       else{
+  //         res.render("Something fail "+err);
+  //       }
+  //   });
+  // });
 });
 
 app.post("/login",function(req, res){
-  const email= req.body.username;
-  const password= req.body.password;
-  //when find a filed automatically the field "password" was decrept
-  User.findOne({email},function(err,foundUser){
-
-    if (err){
-      console.log("ups found a error  "+err);
-    }
-    else {
-        if (foundUser)
-          {
-            bcrypt.compare(password, foundUser.password, function(err, result) {
-            if(result == true)
-              {
-              res.render("secrets");
-              }
-            else{
-              console.log("Error de usuario o contraseña");
-              }
-            });
-          }
-        }
-  });
+  // const email= req.body.username;
+  // const password= req.body.password;
+  // //when find a filed automatically the field "password" was decrept
+  // User.findOne({email},function(err,foundUser){
+  //
+  //   if (err){
+  //     console.log("ups found a error  "+err);
+  //   }
+  //   else {
+  //       if (foundUser)
+  //         {
+  //           bcrypt.compare(password, foundUser.password, function(err, result) {
+  //           if(result == true)
+  //             {
+  //             res.render("secrets");
+  //             }
+  //           else{
+  //             console.log("Error de usuario o contraseña");
+  //             }
+  //           });
+  //         }
+  //       }
+  // });
 
 });
 
